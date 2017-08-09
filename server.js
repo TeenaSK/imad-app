046d8613098fcf3a7ea41d13deacc5bca3d71395
TeenaSK <30652917+TeenaSK@users.noreply.github.com>
2017-08-09 17:58:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,6 +81,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/abc', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'abc.html'));
+});
+
 /*app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
