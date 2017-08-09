96139bf179a8b7953c8a98d36c51359759ed5d7d
TeenaSK <30652917+TeenaSK@users.noreply.github.com>
2017-08-09 13:13:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
-var button= document.getElementById("counter");
+var button = document.getElementById("counter");
 //var counter = 0;
 button.onclick = function(){
     //create request variable
@@ -18,6 +18,19 @@ button.onclick = function(){
     
     request.open('GET','http://teenakanil.imad.hasura-app.io/counter',true);
     request.send(null);
+};
+
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onclick = function(){
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for(var i=0;i<names.length;i++){
+        list = '<li>'+names[i]+'</li>';
+    }
+    var ul = document.getElementById("nameList");
+    ul.innerHTML = list;
 };
 
 /*var element= document.getElementById("main-text");
