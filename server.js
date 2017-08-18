27d37bb03327d2ec46f4f4e80020cfa14ae01e7e
TeenaSK <30652917+TeenaSK@users.noreply.github.com>
2017-08-18 15:59:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
+var config = {
+  user : 'teenakanil',
+  database : 'teenakanil',
+  host : 'http://db.imad.hasura-app.io',
+  port : '5432',
+  password : process.env.DB_PASSWORD
+}
 var app = express();
 app.use(morgan('combined'));
 
@@ -81,8 +88,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/abc', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'abc.html'));
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+  pool.query('SELECT * from tag',function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      }else{
+          res.send(JSON.stringify(result));
+      }
+  })
 });
 
 /*app.get('/:articleName', function (req, res) {
