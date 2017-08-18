de8d18fa03b2312b8c4e742ffc4a7b6fd3b28bd4
TeenaSK <30652917+TeenaSK@users.noreply.github.com>
2017-08-18 16:47:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,9 +99,22 @@ app.get('/test-db', function (req, res) {
   })
 });
 
-/*app.get('/:articleName', function (req, res) {
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+/*app.get('/articles/:articleName', function (req, res) {
+    //var articleName = req.params.articleName;
+    pool.query("SELECT * from article where title='"+req.params.articleName+"'",function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      }else{
+            if(result.rows.length === 0){
+                res.status(404).send('Article not found');
+            }
+            else{
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+      }
+  }));
+    
 });
 
 app.get('/article-one', function (req, res) {
