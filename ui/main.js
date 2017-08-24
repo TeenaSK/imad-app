3c55721f357ca562542296fba8b602a5c1e50017
TeenaSK <30652917+TeenaSK@users.noreply.github.com>
2017-08-24 12:24:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
-var button = document.getElementById("counter");
+/*var button = document.getElementById("counter");
 //var counter = 0;
 button.onclick = function(){
     //create request variable
@@ -17,7 +17,7 @@ button.onclick = function(){
     };
     request.open('GET','http://teenakanil.imad.hasura-app.io/counter',true);
     request.send(null);
-};
+};*/
 
 
 var submit = document.getElementById("submit_btn");
@@ -26,7 +26,9 @@ submit.onclick = function(){
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-                //var names = ['name1','name2','name3','name4'];
+                alert('loggedin successfully');
+                
+                /*//var names = ['name1','name2','name3','name4'];
                 var names = request.responseText;
                 names = JSON.parse(names);
                 var list = '';
@@ -34,14 +36,21 @@ submit.onclick = function(){
                     list = list +'<li>'+names[i]+'</li>';
                 }
                 var ul = document.getElementById("nameList");
-                ul.innerHTML = list;
+                ul.innerHTML = list;*/
+            }else if(request.status === 403){
+                alert('Username/password is incorrect');
+            }else if(request.status === 500){
+                alert('Something went wrong in server');
             }
         }
     };
-    var nameInput = document.getElementById("name");
-    var name = nameInput.value;
-    request.open('GET','http://teenakanil.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://teenakanil.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username, password:password}));
 };
 
 /*var element= document.getElementById("main-text");
